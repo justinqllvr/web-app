d67d4c3fc0208f74ac73d155d1ce9b55b5485a46
Defless <simon.deflesschouwer@mmibordeaux.com>
2019-03-14 12:04:56 +0100

    cities

--- app/models/report.rb
+++ app/models/report.rb
@@ -14,11 +14,26 @@
 #
 
 class Report < ApplicationRecord
+	before_save :find_city
+
 	belongs_to :user
+	belongs_to :city, optional: true
 	has_many :likes, dependent: :destroy
 
 	validates_presence_of :title, :text, :latitude, :longitude
 
+	scope :ordered_by_likes, -> { left_joins(:likes).group(:id).order('COUNT(likes.id) DESC') }
+
 	mount_uploader :picture, ImageUploader
+
+	protected
+
+	def find_city
+		results = Geocoder.search([latitude, longitude])
+		begin
+			self.city = City.where(name: results.first.city).first_or_create
+		rescue
+		end
+	end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_170555) do
+ActiveRecord::Schema.define(version: 2019_03_14_095716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2019_03_12_170555) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
@@ -45,6 +51,8 @@ ActiveRecord::Schema.define(version: 2019_03_12_170555) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.integer "user_id"
+    t.bigint "city_id"
+    t.index ["city_id"], name: "index_reports_on_city_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +85,5 @@ ActiveRecord::Schema.define(version: 2019_03_12_170555) do
 
   add_foreign_key "likes", "reports"
   add_foreign_key "likes", "users"
+  add_foreign_key "reports", "cities"
 end
